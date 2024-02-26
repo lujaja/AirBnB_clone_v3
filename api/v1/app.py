56145c0db9e5c1fe1483209ff4044a39627dfe4a
lujaja <jarzcyber@gmail.com>
2024-02-26 14:45:29 +0300

    api

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python3
+#/usr/bin/python3
 """Status of an API"""
-from flask import Flask
+from flask import Flask, make_response, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -11,9 +11,16 @@ app.url_map.strict_slashes = False
 app.register_blueprint(app_views, url_prefix='/api/v1')
 
 
+
 @app.teardown_appcontext
 def teardown_appcntext(exception):
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(error):
+    """handles error 404"""
+    return make_response(jsonify({'error': 'Not found'}), 404)
 
 
 if __name__ == "__main__":
