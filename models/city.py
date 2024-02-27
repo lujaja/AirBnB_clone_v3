48f3cf3852eaf79e98cf48ea4bc7d7ce6234492e
Wiztech-01 <lynnnyaoke@gmail.com>
2024-02-27 10:27:16 +0300

    update city

--- models/city.py
+++ models/city.py
@@ -3,8 +3,7 @@
 import models
 from models.base_model import BaseModel, Base
 from os import getenv
-import sqlalchemy
-from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy import Column, String, ForeignKey, Float, Integer
 from sqlalchemy.orm import relationship
 
 
@@ -14,10 +13,11 @@ class City(BaseModel, Base):
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place", backref="cities", cascade="delete")
     else:
         state_id = ""
         name = ""
+        places = []
 
     def __init__(self, *args, **kwargs):
         """initializes city"""
